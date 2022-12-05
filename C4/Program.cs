120ab64fced23ac9e44d564e88c8fe0998351509
Irina programmed <krasnova.i@gmail.com>
2022-12-05 13:31:01 +0100

    test

--- C4/Program.cs
+++ C4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите начало числового диапазона: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите окончание числового диапазона: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+for (int i = numberA; i <= numberB; i++)
+{if (i % 2 == 0)
+   {Console.WriteLine(i + " ");}
+}
