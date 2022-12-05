43ef0bb48f51479ad1fe1c8ba1701fc383556eb5
Irina programmed <krasnova.i@gmail.com>
2022-12-05 13:20:02 +0100

    test

---
+++ С1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB)
+   {Console.WriteLine("Число A равно числу B ");}
+  else if (numberB > numberA)
+    {Console.WriteLine("Число B больше числа A ");}
+  else 
+    {Console.WriteLine("Число A больше числа B ");}
